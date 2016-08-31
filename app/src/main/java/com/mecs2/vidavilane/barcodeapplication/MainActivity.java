2b8736e1e059d381438f1aabbd10a245e30f1f79
Hilton Shumway <hillshum@gmail.com>
2016-08-30 21:08:45 -0600

    Inital commmit.

---
+++ app/src/main/java/com/mecs2/vidavilane/barcodeapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.mecs2.vidavilane.barcodeapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // create button to process the puppy image
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        // load image puppy image
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.mipmap.puppy);
+        myImageView.setImageBitmap(myBitmap);
+
+
+        // setup barcode detection
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if (!detector.isOperational()) {
+            TextView txtview = (TextView) findViewById(R.id.txtContent);
+            txtview.setText("Could not set up the detector!");
+            return;
+        }
+        else{
+            TextView txtview = (TextView) findViewById(R.id.txtContent);
+            txtview.setText("Good to GO!!!");
+        }
+
+        // detect the barcode
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        // decode the barcode
+        Barcode thisCode = barcodes.valueAt(0);
+        TextView txtView = (TextView) findViewById(R.id.txtContent);
+        txtView.setText(thisCode.rawValue);
+    }
+}
